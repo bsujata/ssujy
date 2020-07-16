b2383a18f5c97f7a4ce0206f56105e8a2534673f
suja <bsujata@yahoo.com>
2020-07-16 19:47:19 +0530

    changed

--- src/sum/sum1.java
+++ src/sum/sum1.java
@@ -2,9 +2,11 @@ package sum;
 
 public class sum1 {
 	public static void main (String args[]) {
-		int a=98,b=47, sum1;
+		int a=98,b=47, sum1,sub1;
 		sum1=a+b;
+		sub1=a-b;
 		System.out.println(sum1);
+		System.out.println(sub1);
 		
 	}
 		
